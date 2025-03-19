66abd0f1ad9e73adb40bca88fa185f62547be847
Gesley Marinho <gesleymarinho@gmail.com>
2025-03-19 13:27:30 -0300

    Aula 211 Banco de dados, criado conexão com o banco de dados SQLITE, insert no banco e leitura da tabela

---
+++ src/PacoteAulasBancoDeDadosJDBC/Program.java
@@ -0,0 +1,40 @@
+package PacoteAulasBancoDeDadosJDBC;
+
+
+import PacoteAulasBancoDeDadosJDBC.ConexaoBD.DB;
+import PacoteAulasBancoDeDadosJDBC.ConexaoBD.DbException;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Program {
+    public static void main(String[] args)  {
+        Connection conn = null;
+
+
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM DEPARTMENT");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + " , "+ rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeResulSet(rs);
+            DB.closeStatment(st);
+            DB.closeConnection();
+        }
+
+    }
+}
